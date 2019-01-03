816d7386b93134f932e692329db38da5d85ad5be
ersaurav03 <er.saurav03@gmail.com>
2019-01-03 18:56:38 +0530

    applied change on values and workdone works

--- src/components/values.vue
+++ src/components/values.vue
@@ -8,14 +8,14 @@
 
 			<div class="col-sm-6 col-md-6 col-lg-6  text-light">
 				<div class="row">
-					<div class="col-sm-6 col-md-6 p-5  border border-light bg-dark ">
-						<div class="d-flex justify-content-start"><i class="fas fa-anchor fa-2x"></i></div><br>
-						<div class="d-flex justify-content-start"><b text-light>Branding Identity</b></div>
-						<div class="d-flex justify-content-start"><i class="fa fa-times" aria-hidden="true"></i></div>
-						<div class="d-flex justify-content-center"><p><h6>Lorem ipsum dolor sit amet consectetur adipcing elit rivamus a aliquet interdum maurisaliquet felis interdume</h6></p></div>
+					<div class="col-sm-6 col-md-6 p-5  border border-light bg-dark " v-for="arr1 in arr">
+						<div class="d-flex justify-content-start"><i :class="arr1.i1"></i></div><br>
+						<div class="d-flex justify-content-start"><b text-light>{{arr1.h}}</b></div>
+						<div class="d-flex justify-content-start"><i :class="arr1.i2" aria-hidden="true"></i></div>
+						<div class="d-flex justify-content-center"><p><h6>{{arr1.p}}</h6></p></div>
 					</div>
 
-					<div class="col-sm-6 col-md-6 p-5 bg-dark border border-light "> 
+<!-- 					<div class="col-sm-6 col-md-6 p-5 bg-dark border border-light "> 
 						<div class="d-flex justify-content-start"><i class="fas fa-bell fa-2x"></i></div><br>
 						<div class="d-flex justify-content-start"><b>Marketing Services</b></div>
 						<div class="d-flex justify-content-start"><i class="fa fa-times" aria-hidden="true"></i></div>
@@ -49,7 +49,7 @@
 						<div class="d-flex justify-content-start"><i class="fa fa-times" aria-hidden="true"></i></div>
 						<div class="d-flex justify-content-center"><p><h6>Lorem ipsum dolor sit amet consectetur adipcing elit rivamus a aliquet interdum maurisaliquet felis interdume</h6></p></div>
 					</div>
-
+ -->
 				</div>
 			</div>
 		</div>
@@ -65,3 +65,20 @@ background-position: center;
 background-repeat: no-repeat;
 }
 </style>
+
+<script>
+	export default{
+		data:function(){
+			return{
+				arr:[
+				{i1:'fas fa-anchor fa-2x',h:'Branding Identity',i2:'fa fa-times',p:'Lorem ipsum dolor sit amet consectetur adipcing elit rivamus a aliquet interdum maurisaliquet felis interdume'},
+				{i1:'fas fa-bell fa-2x',h:'Marketing Services',i2:'fa fa-times',p:'Lorem ipsum dolor sit amet consectetur adipcing elit rivamus a aliquet interdum maurisaliquet felis interdume'},
+				{i1:'fas fa-edit fa-2x',h:'Unlimited Colors',i2:'fa fa-times',p:'Lorem ipsum dolor sit amet consectetur adipcing elit rivamus a aliquet interdum maurisaliquet felis interdume'},
+				{i1:'fas fa-cogs fa-2x',h:'Free Support & Updfates',i2:'fa fa-times',p:'Lorem ipsum dolor sit amet consectetur adipcing elit rivamus a aliquet interdum maurisaliquet felis interdume'},
+				{i1:'fas fa-link fa-2x',h:'SEO Ready',i2:'fa fa-times',p:'Lorem ipsum dolor sit amet consectetur adipcing elit rivamus a aliquet interdum maurisaliquet felis interdume'},
+				{i1:'fas fa-layer-group fa-2x',h:'Ma Styles Available',i2:'fa fa-times',p:'Lorem ipsum dolor sit amet consectetur adipcing elit rivamus a aliquet interdum maurisaliquet felis interdume'}
+				]
+			}
+		}
+	}
+</script>
--- src/components/workdone.vue
+++ src/components/workdone.vue
@@ -2,12 +2,12 @@
 <div class="container">
 	<div class="row">
 		<div class="col-sm-1 col-md-1 col-lg-1 "></div>
-		<div class=" col-sm-6 col-md-2 pt-5 trackrecord">
-			<div class="d-flex justify-content-center"><h1>99</h1></div>
-			<div class="d-flex justify-content-center"><h6>Project Completed</h6></div>
+		<div class=" col-sm-6 col-md-2 pt-5 trackrecord" v-for="arr1 in arr">
+			<div class="d-flex justify-content-center"><h1>{{arr1.p}}</h1></div>
+			<div class="d-flex justify-content-center"><h6>{{arr1.d}}</h6></div>
 		</div>
 
-		<div class=" col-sm-6 col-md-2 pt-5 trackrecord">
+<!-- 		<div class=" col-sm-6 col-md-2 pt-5 trackrecord">
 			<div class="d-flex justify-content-center"><h1>120</h1></div>
 			<div class="d-flex justify-content-center"> <h6>Happy Clients</h6></div>
 		</div>
@@ -22,13 +22,27 @@
 		<div class="col-sm-6 col-md-2 pt-5 trackrecord">
 			<div class="d-flex justify-content-center"><h1>3500</h1></div>
 			<div class="d-flex justify-content-center"> <h6>Ratings</h6></div>
-		</div>
+		</div> -->
 
 	</div>
 </div>
 </template>
 
-
+<script>
+	export default{
+		data:function(){
+			return{
+				arr:[
+				{p:'99',d:'Project Completed'},
+				{p:'120',d:'Happy Clients'},
+				{p:'75',d:'Cups of Coffee'},
+				{p:'105',d:'Awards Winner'},
+				{p:'3500',d:'Ratings'}
+				]
+			}
+		}
+	}
+</script>
 <style>
 div.trackrecord
 {
