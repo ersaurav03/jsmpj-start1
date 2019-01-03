efc41aca1bf9e9af4ee4b16bb6faf4d19d1d94b8
ersaurav03 <er.saurav03@gmail.com>
2019-01-03 19:00:54 +0530

    applied change on brands and ourteam works

--- src/components/brands.vue
+++ src/components/brands.vue
@@ -9,6 +9,9 @@
 				<div class="col-sm-6 col-md-6 col-lg-2 ocust  border border-light " v-for="arr1 in arr">
 					<div class="d-flex justify-content-center "><img  :src="arr1" class="img-responsive" height="100px" width="150px"></div>
 				</div>
+				<div class="col-sm-6 col-md-6 col-lg-1 ocust  border border-light ">
+                 	
+                 </div>
 <!-- 				<div class="col-sm-6 col-md-6 col-lg-2 ocust  border border-light ">
 					<div class="d-flex justify-content-center"><img  src="../../public/images/19.png" class="img-responsive" height="100px" width="150px"></div>
 				</div>
