b83e9baabd5414f0ef9116d32eff7083f923de7a
ersaurav03 <er.saurav03@gmail.com>
2019-01-11 17:35:41 +0530

    Removed errors JSMPJ

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,79 +1,79 @@
-<template>
-<div class="About">
-<navupper></navupper>
-<navlower></navlower>	
-<div class="container">
-<div class="card">
-  <div class="card-header d-flex justify-content-start">
-   <div> <strong>{{p | toUppercase}}</strong></div>
-	<div class="input-group d-flex justify-content-end">
-	<div class="input-group-append">
-		<input v-model="filterText" type="text" class="form-control-sm" placeholder="Search">
-		<button class="btn btn-sm btn-outline-dark bg-dark text-light " type="button">
-        <i class="fa fa-search" aria-hidden="true"></i>
-    </button>
-	</div>
-	</div>  
+  <template>
+    <div class="About">
+      <navupper></navupper>
+      <navlower></navlower>	
+      <div class="container">
+        <div class="card">
+          <div class="card-header d-flex justify-content-start">
+           <div> <strong>{{p | toUppercase}}</strong></div>
+           <div class="input-group d-flex justify-content-end">
+             <div class="input-group-append">
+              <input v-model="filterText" type="text" class="form-control-sm" placeholder="Search">
+              <button class="btn btn-sm btn-outline-dark bg-dark text-light " type="button">
+                <i class="fa fa-search" aria-hidden="true"></i>
+              </button>
+            </div>
+          </div>  
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Services are Available in Our Company</h5>
+          <ol class="card-text">
+           <li v-bind:key="fruit" v-for="fruit in filteredFruits">
+            <p>{{fruit}}</p>
+          </li>
+        </ol>
+        <router-link to="/" class="btn btn-primary">Go To Home</router-link>
+      </div>
+      <div class="card-footer text-muted">
+        Last Update About Product 2 days ago
+      </div>
+    </div>
   </div>
-  <div class="card-body">
-    <h5 class="card-title">Services are Available in Our Company</h5>
-    <ol class="card-text">
-    	<li v-for="fruit in filteredFruits">
-    		<p>{{fruit}}</p>
-    	</li>
-    </ol>
-    <router-link to="/" class="btn btn-primary">Go To Home</router-link>
-  </div>
-  <div class="card-footer text-muted">
-  Last Update About Product 2 days ago
-  </div>
-</div>
-</div>
-<footerup></footerup>
-<footerdown></footerdown>
+  <footerup></footerup>
+  <footerdown></footerdown>
 </div>
 </template>
 
 
 <script>
-import navupper from '../components/navupper.vue'
-import navlower from '../components/navlower.vue'
-import footerup from '../components/footerup'
-import footerdown from '../components/footerdown'
-import whatweoffer from '../components/whatweoffer'
+  import navupper from '../components/navupper.vue'
+  import navlower from '../components/navlower.vue'
+  import footerup from '../components/footerup'
+  import footerdown from '../components/footerdown'
+  // import whatweoffer from '../components/whatweoffer'
 
-export default {
-name: 'About',
-components: {
- navupper,
-navlower,
-whatweoffer,
-footerup,
-footerdown
+  export default {
+    name: 'About',
+    components: {
+     navupper,
+     navlower,
+  // whatweoffer,
+  footerup,
+  footerdown
 },
 data() {
-return{
-	filterText:'',
+  return{
+  filterText:'',
 	p:'Product',
-	fruits:['Advisory','Financial','Inforation & Technology','Banking','Insurance','Mutual Funds','Forex','Share Broking']
-}
+  fruits:['Advisory','Financial','Inforation & Technology','Banking','Insurance','Mutual Funds','Forex','Share Broking']
+  }
 },
 methods:{
-navigateToHome(){
+  navigateToHome(){
     this.$router.push('/');
-}
+  }
 },
 computed:{
-	filteredFruits(){
-		return this.fruits.filter((element) => {
-            return element.match(this.filterText);
-		});
-	}
+ filteredFruits(){
+  return this.fruits.filter((element) => {
+    return element.match(this.filterText);
+  });
+}
 },
 filters:{
-	toUppercase(value){
-		return value.toUpperCase();
-	}
+ toUppercase(value){
+  return value.toUpperCase();
+}
 }
 }
 </script>
@@ -81,15 +81,15 @@ filters:{
 <style>
 .w1
 {
-	width: 100%;
+ width: 100%;
 }
 .img1
 {
-	background: url("../../public/images/slider0.png");
-	height: 1000px;
-	width: 100%;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
+ background: url("../../public/images/slider0.png");
+ height: 1000px;
+ width: 100%;
+ background-position: center;
+ background-repeat: no-repeat;
+ background-size: cover;
 }
 </style>
--- src/views/pages.vue
+++ src/views/pages.vue
@@ -1,83 +1,83 @@
-<template>
-<div class="pages">
-<navupper></navupper>
-<navlower></navlower>
-<div class="container">
-<div class="card">
-  <div class="card-header d-flex justify-content-start">
-   <div> <strong>{{p | toUppercase}}</strong></div>
-	<div class="input-group d-flex justify-content-end">
-	<div class="input-group-append">
-		<input v-model="filterText" type="text" class="form-control-sm" placeholder="Search">
-		<button class="btn btn-sm btn-outline-dark bg-dark text-light " type="button">
-        <i class="fa fa-search" aria-hidden="true"></i>
-    </button>
-	</div>
-	</div>  
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Services are Available in Our (Company It is done though global mixins) </h5>
-    <ol class="card-text">
-    	<li v-for="fruit in filteredFruits">
-    		<p>{{fruit}}</p>
-    	</li>
-    </ol>
-    <router-link to="/" class="btn btn-primary">Go To Home</router-link>
-  </div>
-  <div class="card-footer text-muted">
-  Last Update About Product 2 days ago
-  </div>
-</div>
-</div>	
-<footerup></footerup>
-<footerdown></footerdown>
-</div>
-</template>
+    <template>
+      <div class="pages">
+        <navupper></navupper>
+        <navlower></navlower>
+        <div class="container">
+          <div class="card">
+            <div class="card-header d-flex justify-content-start">
+             <div> <strong>{{p | toUppercase}}</strong></div>
+             <div class="input-group d-flex justify-content-end">
+              <div class="input-group-append">
+                <input v-model="filterText" type="text" class="form-control-sm" placeholder="Search">
+                <button type="button" class="btn btn-sm btn-outline-dark bg-dark text-light">
+                  <i class="fa fa-search" aria-hidden="true"></i>
+                </button>
+              </div>
+            </div>  
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">Services are Available in Our (Company It is done though global mixins) </h5>
+            <ol class="card-text">
+              <li v-bind:key="fruit" v-for="fruit in filteredFruits">
+               <p>{{fruit}}
+               </p></li>
+             </ol>
+             <router-link to="/" class="btn btn-primary">Go To Home</router-link>
+           </div>
+           <div class="card-footer text-muted">
+            Last Update About Product 2 days ago
+          </div>
+        </div>
+      </div>	
+      <footerup></footerup>
+      <footerdown></footerdown>
+    </div>
+  </template>
 
-<script>
-import navupper from '../components/navupper.vue'
-import navlower from '../components/navlower.vue'
-import footerup from '../components/footerup'
-import footerdown from '../components/footerdown'
-import whatweoffer from '../components/whatweoffer'
-import { mixxx } from '../Mixing';
-export default {
-mixins:[mixxx],
-name: 'About',
-components: {
- navupper,
-navlower,
-whatweoffer,
-footerup,
-footerdown
-},
+  <script>
+    import navupper from '../components/navupper.vue'
+    import navlower from '../components/navlower.vue'
+    import footerup from '../components/footerup'
+    import footerdown from '../components/footerdown'
+    // import whatweoffer from '../components/whatweoffer'
+    import { mixxx } from '../Mixing';
+    export default {
+      mixins:[mixxx],
+      name: 'About',
+      components: {
+        navupper,
+        navlower,
+    // whatweoffer,
+    footerup,
+    footerdown
+  },
 
-methods:{
-navigateToHome(){
-    this.$router.push('/');
-}
-},
+  methods:{
+    navigateToHome(){
+      this.$router.push('/');
+    }
+  },
 
-filters:{
-	toUppercase(value){
-		return value.toUpperCase();
-	}
-}
+  filters:{
+    toUppercase(value){
+      return value.toUpperCase();
+    }
+  }
 }
 </script>
 
 <style>
 .w1
 {
-	width: 100%;
+  width: 100%;
 }
 .img1
 {
-	background: url("../../public/images/slider0.png");
-	height: 1000px;
-	width: 100%;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
+  background: url("../../public/images/slider0.png");
+  height: 1000px;
+  width: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 </style>
